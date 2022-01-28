cbc8e59d6cf99cea94076d363ee218d76e05fd2d
kmrdknrd <konrad.mikalauskas@gmail.com>
2022-01-28 21:08:27 +0100

    Update cheat.py

--- cheat.py
+++ cheat.py
@@ -7,9 +7,12 @@ def cheat(exercise):
     if exercise==1.8:
         result = (np.arange(1, 17) ** 2).reshape(2, 2, 2, 2)
         print(result)
-    elif exercise==1.9:
-        result = pd.read_csv('InsectSprays.csv').to_dict()
-        print(result.keys())
+    elif exercise==2.1:
+        two_dice = np.random.choice(np.arange(1, 7), 2, replace=True)
+        if sum(two_dice) % 2 == 0:
+            result = min(two_dice) * sum(two_dice)
+        else:
+            result = 3 * sum(two_dice)
     elif exercise==2.2:
         result = np.array([])
         for i in np.arange(3, 18, 2):
@@ -21,6 +24,8 @@ def cheat(exercise):
         plt.figure()
         sns.histplot(data, x='Sex', hue='Survived', multiple='stack', color=('red', 'cyan'))
         plt.savefig("Q3.2P.2.stacked.png", dpi=300)
+        return
     else:
         print("welp")
+        return
     return result
